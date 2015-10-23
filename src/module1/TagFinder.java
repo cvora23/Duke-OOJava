6bd638bc37d3bef96e399dbe5e6e3ffdb18d72c1
Chintan Vora <cvora@center4mi.org>
2015-10-22 21:17:20 -0700

    Module 2 Assignment

---
+++ src/module1/TagFinder.java
@@ -0,0 +1,69 @@
+/**
+ * Finds a protein within a strand of DNA represented as a string of c,g,t,a letters.
+ * A protein is a part of the DNA strand marked by start and stop codons (DNA triples)
+ * that is a multiple of 3 letters long.
+ *
+ * @author Duke Software Team 
+ */
+
+package module1;
+
+import edu.duke.*;
+
+import java.io.*;
+
+public class TagFinder {
+	
+	public String findProtein(String dna) {
+		String lowerCasedna = dna.toLowerCase();
+		int start = lowerCasedna.indexOf("atg");
+		if (start == -1) {
+			return "";
+		}
+		
+		int tagStopCodon = lowerCasedna.indexOf("tag", start+3);
+		int tgaStopCodon = lowerCasedna.indexOf("tga", start+3);
+		int taaStopCodon = lowerCasedna.indexOf("taa", start+3);
+
+		//"CASE FOR TAG STOP CODON"
+		if ((tagStopCodon - start) % 3 == 0) {
+			return lowerCasedna.substring(start, tagStopCodon+3);
+		}
+		
+		//"CASE FOR TGA STOP CODON"
+		else if((tgaStopCodon-start) % 3 == 0){
+			return lowerCasedna.substring(start, tgaStopCodon+3);
+		}
+		
+		//"CASE FOR TAA STOP CODON"
+		else if((taaStopCodon-start) % 3 == 0){
+			return lowerCasedna.substring(start, taaStopCodon+3);
+		}
+		else {
+			return "";
+		}
+		
+	}
+	
+	public void testing() {
+		
+		String test1 = "AATGCTAGTTTAAATCTGA";
+		String test2 = "ataaactatgttttaaatgt";
+		String test3 = "acatgataacctaag";
+
+		
+		String result = findProtein(test1);
+		System.out.println(result);
+		result = findProtein(test2);
+		System.out.println(result);
+		result = findProtein(test3);
+		System.out.println(result);
+	}
+	
+	
+	public static void main(String[] args) {
+		TagFinder tagFinder = new TagFinder();
+		tagFinder.testing();
+    }
+	
+}
---
+++ src/module1/YoutubeLinkFinder.java
@@ -0,0 +1,44 @@
+package module1;
+
+import edu.duke.URLResource;
+
+public class YoutubeLinkFinder {
+
+		private static String UrlLoc = "http://www.dukelearntoprogram.com/course2/data/manylinks.html";
+		
+		public void printYoutubeLinks(){
+			URLResource res = new URLResource(UrlLoc);
+			for (String line : res.words()) {
+				//System.out.println(line);
+				printIfYoutubeLinks(line);
+			}
+		}
+		
+		public void printIfYoutubeLinks(String ipString){
+			
+			// Given a line of string.
+			// 1: "Find youtube.com pattern from line"
+			// 2: If found go find " before youtube.com and " after youtube.com.
+			// 3: Between these starTag and endTag is the youtube link. (Take care about case sensitivity)
+			
+			String ipStringPostProc = 	ipString.toLowerCase();		
+			int youtubeIndex = ipStringPostProc.indexOf("youtube.com");
+			if(youtubeIndex == -1){
+				return;
+			}else{
+				int startIndex = 0,endIndex = 0;
+				startIndex = ipStringPostProc.lastIndexOf('"', youtubeIndex);
+				endIndex = ipStringPostProc.indexOf('"', youtubeIndex);
+				System.out.println(ipString.substring(startIndex+1,endIndex));
+			}
+			
+		}
+		
+		public static void main(String[] args) {
+			YoutubeLinkFinder youtubeLinkFinder = new YoutubeLinkFinder();
+			youtubeLinkFinder.printYoutubeLinks();
+	    }
+		
+		
+	
+}
